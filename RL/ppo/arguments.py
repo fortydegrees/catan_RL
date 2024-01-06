e2bac811e37d3f54de8f54a16213d83db07ff561
fortydegrees <david@david-pratt.com>
2024-01-06 12:00:11 +0000

    batch size

--- RL/ppo/arguments.py
+++ RL/ppo/arguments.py
@@ -27,18 +27,17 @@ def get_args():
     parser.add_argument(
         '--entropy-coef-final', type=float, default=0.005
     )
-    #scaled by 5
     parser.add_argument(
-        '--entropy-coef-start-anneal', type=int, default=1000
+        '--entropy-coef-start-anneal', type=int, default=500
     )
     parser.add_argument(
-        '--entropy-coef-end-anneal', type=int, default=3000
+        '--entropy-coef-end-anneal', type=int, default=1500
     )
     parser.add_argument(
-        '--dense-reward-anneal-start', type=int, default=1
+        '--dense-reward-anneal-start', type=int, default=1000
     )
     parser.add_argument(
-        '--dense-reward-anneal-end', type=int, default=1000
+        '--dense-reward-anneal-end', type=int, default=2000
     )
     parser.add_argument(
         '--value-loss-coef', type=float, default=1.0
@@ -58,7 +57,7 @@ def get_args():
     #The number of decisions the agent being trained (i.e. the agent with the most recent policy) should make in each game on each process.
     #default 200 (for 4p)
     parser.add_argument(
-        '--num-steps', type=int, default=200
+        '--num-steps', type=int, default=1000
     )
     parser.add_argument(
         '--truncated-seq-len', type=int, default=10
@@ -74,7 +73,7 @@ def get_args():
     #So we have 2000 samples (5 * 10 * 600) = 30,000 / 15 = 2,000
     #default was 200
     parser.add_argument(
-        '--num-mini-batch', type=int, default=12
+        '--num-mini-batch', type=int, default=64
     )
     parser.add_argument(
         '--clip-param', type=float, default=0.2
@@ -96,15 +95,15 @@ def get_args():
     )
     #changed to scale
     parser.add_argument(
-        '--add-policy-every', type=int, default=8, help='add new policy every this many updates'
+        '--add-policy-every', type=int, default=4, help='add new policy every this many updates'
     )
     #changed to scale
     parser.add_argument(
-        '--update-opponent-policies-every', type=int, default=2
+        '--update-opponent-policies-every', type=int, default=1
     )
     #default = 25
     parser.add_argument(
-        '--eval-every', type=int, default=50
+        '--eval-every', type=int, default=25
     )
     parser.add_argument(
         '--num-eval-episodes', type=int, default=128
